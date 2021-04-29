eb9d6d27d991137f3458b2c270ab84fa3b10b76a
CorentinDallenogare <he201885@students.ephec.be>
2021-04-29 17:47:27 +0200

    ajouts des commentaires web externe

--- dmz/web/b2b/index.php
+++ dmz/web/b2b/index.php
@@ -1,5 +1,6 @@
 <?php
-    $connexion = mysqli_connect('135.125.101.214:3306', 'root', 'test', 'myDb')or die('Erreur de connexion');
+    $connexion = mysqli_connect('ip', 'user', 'mdp', 'myDb')or die('Erreur de connexion');
+    //connexion à la base de donnée
 ?>
 <html lang = "fr">
 	<head>
@@ -10,31 +11,32 @@
 	<body>
 		<h1> site web b2b de Woodytoys </h1>
 		<?php
-				$test = mysqli_query($connexion,'SELECT * FROM objets;');
+				$test = mysqli_query($connexion,'SELECT * FROM objets;'); //permet de tout sélectionner dans la table objets
 			while($result = mysqli_fetch_row($test)) {
-				 print '<p>Article : '.$result[0].'. Prix : '.$result[1].'€</p>';
+				 print '<p>Article : '.$result[0].'. Prix : '.$result[1].'€</p>'; //ajoute différents paragraphes
 			};
 
-            if(isset($_POST['submit']))
+            if(isset($_POST['submit'])) //si on a submit
             {
-                $objet = $_POST['objet'];
+                $objet = $_POST['objet']; //assignation des valeurs du formulaire à des variables
                 $prix = $_POST['prix'];
 
                 $insert = mysqli_query($connexion,"INSERT INTO `objets`(`name`, `prix`) VALUES ('$objet','$prix')");
+                //insertion dans la base de donnée
 
                 if(!$insert)
                 {
-                    echo "erreur";
+                    echo "erreur"; //affiche erreur si ça n'a pas pu ajouter dans la base de donnée
                 }
                 else
                 {
-                    echo "Donnée bien envoyez. Rechargez la page";
+                    echo "Donnée bien envoyez. Rechargez la page"; //affiche si les données ont été ajoutées
 
                 }
             }
 			 mysqli_close($connexion);
 		?>
-        <form method="POST">
+        <form method="POST"> <!-- formulaire d'insertion -->
             objet <input type="text" name="objet" placeholder="objet" Required>
             <br/>
             prix : <input type="number" name="prix" placeholder="prix" Required>
